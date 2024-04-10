946c978d5ea8e61be6393781eb179e5d3e63cf5b
playfulCloud <godlygener@gmail.com>
2024-04-11 01:12:47 +0200

    initial

---
+++ alfa/src/main/java/dream/track/alfa/AlfaApplication.java
@@ -0,0 +1,13 @@
+package dream.track.alfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaApplication.class, args);
+	}
+
+}
---
+++ alfa/src/test/java/dream/track/alfa/AlfaApplicationTests.java
@@ -0,0 +1,13 @@
+package dream.track.alfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
