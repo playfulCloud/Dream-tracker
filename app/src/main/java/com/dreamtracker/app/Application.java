d0d68f00c8fadbd855ff33f4ca37dce97cc22ece
NimboStratus <playfulCloud@proton.me>
2024-04-26 23:24:53 +0200

    feat: General code structure for Habit and goal

--- app/src/main/java/com/dreamtracker/app/Application.java
+++ app/src/main/java/com/dreamtracker/app/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
