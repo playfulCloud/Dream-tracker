83260943ee93cf50ddded8c01fc92cc94964b75c
NimboStratus <playfulCloud@proton.me>
2024-04-29 13:22:42 +0200

    feat: Delete habit endpoint implementation

--- app/src/main/java/com/dreamtracker/app/service/HabitService.java
+++ app/src/main/java/com/dreamtracker/app/service/HabitService.java
@@ -15,6 +15,8 @@ public interface HabitService {
     Optional<Habit> save(Habit habit);
     Optional<Habit>findHabitById(UUID id);
     void deleteById(UUID id);
+    boolean delete(UUID id);
+    boolean existsById(UUID id);
     Optional<HabitTrack>trackTheHabit(HabitTrackingRequest habitTrackingRequest);
     Optional<List<HabitTrack>>getHabitTrack(UUID id);
     HabitResponse createHabit(HabitRequest habitRequest);
