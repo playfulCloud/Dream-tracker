d9ca4173414fd31f9139cadf268d229fe0699808
NimboStratus <playfulCloud@proton.me>
2024-05-01 18:36:28 +0200

    feat: unit test structure

---
+++ app/src/test/java/com/dreamtracker/app/service/impl/HabitTrackServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.dreamtracker.app.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HabitTrackServiceImplTest {
+
+    @Test
+    void save() {
+      }
+
+    @Test
+    void findHabitTrackById() {
+      }
+
+    @Test
+    void deleteById() {
+      }
+
+    @Test
+    void getAllTracksOfHabit() {
+      }
+
+    @Test
+    void trackTheHabit() {
+      }
+}
---
+++ app/src/test/java/com/dreamtracker/app/service/impl/UserServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.dreamtracker.app.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserServiceImplTest {
+
+    @Test
+    void save() {
+      }
+
+    @Test
+    void createSampleUser() {
+      }
+
+    @Test
+    void findById() {
+      }
+}
