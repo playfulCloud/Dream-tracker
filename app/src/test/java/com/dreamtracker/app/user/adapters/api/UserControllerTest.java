33083db2cb3ffd7cf597c99ac222d7f3dc15f578
NimboStratus <playfulCloud@proton.me>
2024-06-05 21:52:14 +0200

    resolved

--- app/src/test/java/com/dreamtracker/app/user/adapters/api/UserControllerTest.java
+++ app/src/test/java/com/dreamtracker/app/user/adapters/api/UserControllerTest.java
@@ -1,9 +1,6 @@
 package com.dreamtracker.app.user.adapters.api;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import com.dreamtracker.app.configuration.TestPostgresConfiguration;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = TestPostgresConfiguration.class)
 class UserControllerTest {}
