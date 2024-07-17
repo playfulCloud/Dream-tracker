f14f5d91f0ad890e7da91e32deb9ef5ecf4f00b5
NimboStratus <playfulCloud@proton.me>
2024-07-17 22:56:17 +0200

    fix: Fix the review comments

--- app/src/main/java/com/dreamtracker/app/habit/adapters/api/HabitTrackResponse.java
+++ app/src/main/java/com/dreamtracker/app/habit/adapters/api/HabitTrackResponse.java
@@ -3,20 +3,8 @@ package com.dreamtracker.app.habit.adapters.api;
 import lombok.Builder;
 
 import java.time.OffsetDateTime;
-import java.util.Objects;
 
 @Builder
 public record HabitTrackResponse(OffsetDateTime date, String status) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        HabitTrackResponse that = (HabitTrackResponse) o;
-        return Objects.equals(status, that.status) && Objects.equals(date, that.date);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(date, status);
-    }
 }
